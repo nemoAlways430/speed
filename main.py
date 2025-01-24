a195322d65b41d0f4782f8b56f50b8622f9e7d57
nemoAlways430 <shuaishuai1S!>
2025-01-24 10:10:14 +0800

    test files

---
+++ main.py
@@ -0,0 +1,5 @@
+# 执行 getPictures.py, 存好图片 （目前其中是Bill提供的示例代码）
+
+# 识别图片，判断新的行动行为 （核心代码）
+
+# 向小车的mqtt server发送指令 （参考mqttToCar.py）
---
+++ mqttToCar.py
@@ -0,0 +1,29 @@
+import paho.mqtt.client as mqtt  
+
+from CarDriver import car
+
+client_id = 'car'
+broker = "192.168.1.7" 
+port = 1883 
+keepalive = 60 
+
+CarState = 0
+
+TopicMoveV = "car/moveV" 
+TopicMoveH = "car/moveH" 
+TopicMoveT = "car/moveT" 
+TopicStop = "car/stop" 
+
+TopicArmF = "car/arm/F" 
+TopicArmR = "car/arm/R" 
+TopicArmCO = "car/arm/CO" 
+TopicArmCC = "car/arm/CC"
+TopicCarStart = "car/Start"
+
+mqttc = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2)
+mqttc.user_data_set([])
+mqttc.connect(broker, port, keepalive)
+mqttc.loop_forever()
+
+# test 慎重运行 保证小车在安全的位置
+# mqttc.publish(TopicMoveV, 5)  
